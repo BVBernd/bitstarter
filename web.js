f3f7b3afd9415396a1ccd34f88b6ce8c0cc461d3
BVBernd <b.p.schmid@gmail.com>
2013-07-18 16:22:56 +0000

    include require fs

--- web.js
+++ web.js
@@ -1,9 +1,10 @@
 var express = require('express');
+var fs = require('fs') 
 
 var app = express.createServer(express.logger());
 
-//var test = fs.readFileSync('index.html');
-//var text = test.toString();
+var test = fs.readFileSync('index.html');
+var text = test.toString();
 var text2 = "this is my text\n";
 
 app.get('/', function(request, response) {
