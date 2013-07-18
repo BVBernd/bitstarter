59d178a572faed4443eeba4cd8d9011e6cd7461b
BVBernd <b.p.schmid@gmail.com>
2013-07-18 12:07:53 +0000

    test whether variable works

--- web.js
+++ web.js
@@ -2,8 +2,11 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+//var text = fs.readFileSync(index.html)
+var text = "this is my text"
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
