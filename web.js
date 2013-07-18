b9731879b3ce548d1038704eefdf5760acc91a45
BVBernd <b.p.schmid@gmail.com>
2013-07-18 13:22:09 +0000

    Read from file

--- web.js
+++ web.js
@@ -1,15 +1,11 @@
-var express = require('express');
-
-var app = express.createServer(express.logger());
-
-var text = fs.readFileSync('index.html')
-//var text = "this is my text"
-
-app.get('/', function(request, response) {
-  response.send(text);
-});
-
-var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
-});
+ReferenceError: buf is not defined
+    at repl:1:2
+    at REPLServer.self.eval (repl.js:112:21)
+    at Interface.<anonymous> (repl.js:239:12)
+    at Interface.EventEmitter.emit (events.js:95:17)
+    at Interface._onLine (readline.js:202:10)
+    at Interface._line (readline.js:531:8)
+    at Interface._ttyWrite (readline.js:767:16)
+    at ReadStream.onkeypress (readline.js:99:10)
+    at ReadStream.EventEmitter.emit (events.js:98:17)
+    at emitKey (readline.js:1095:12)
